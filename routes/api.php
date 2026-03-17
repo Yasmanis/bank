eba087c7b60aef04ffdad283b01598f5e461fb59
carlos.rodriguez <carlos.rodriguez@dev.com>
2026-03-17 13:38:08 -0400

    agregando capa de seguridad, agregando servicio de logger

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
 //TODO agregar middleware verify.signature cuando se compile
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum','verify.signature'])->group(function () {
     Route::post('/refresh', [\App\Http\Controllers\AuthController::class, 'refresh']);
     Route::get('/check-time', fn() => now()->toDateTimeString());
     Route::prefix('list')->group(function () {
